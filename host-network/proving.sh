205250035376b72bd14580c16aa73ebf5b02854f
vanhger <vietanhpg2003@gmail.com>
2025-04-11 16:00:42 +0700

    fix: fix running script

--- host-network/proving.sh
+++ host-network/proving.sh
@@ -1,5 +1,7 @@
-BASEDIR=$(cd $(dirname $0); pwd)
+#!/bin/bash -e
 
+BASEDIR=$(cd $(dirname $0); pwd)
+export ZKM_PROVER=${ZKM_PROVER-"network"}
 export RUST_LOG=${RUST_LOG-info}
 export SEG_SIZE=${SEG_SIZE-262144}
 export OUTPUT_DIR=${BASEDIR}/output
@@ -7,17 +9,18 @@ export EXECUTE_ONLY=false
 
 ##network proving
 export CA_CERT_PATH=${BASEDIR}/tool/ca.pem
-export CERT_PATH=
-export KEY_PATH=
-export PROOF_NETWORK_PRVKEY=  ##The private key corresponding to the public key when registering in the https://www.zkm.io/apply
+#export CERT_PATH=
+#export KEY_PATH=
+##The private key corresponding to the public key when registering in the https://www.zkm.io/apply
+#export PROOF_NETWORK_PRVKEY=
 export ENDPOINT=https://152.32.186.45:20002    ##the test entry of zkm proof network
-export DOMAIN_NAME=
+#export DOMAIN_NAME=
 
-export GOAT_WITHDRAW_TXID=
-export WITHDRAW_CONTRACT_ADDRESS=
-export WITHDRAW_MAP_BASE_KEY=
-export WITHDRAW_MAP_INDEX=
-export PEG_IN_TXID=
-export JSON_PATH=
+#export GOAT_WITHDRAW_TXID=
+#export WITHDRAW_CONTRACT_ADDRESS=
+#export WITHDRAW_MAP_BASE_KEY=
+#export WITHDRAW_MAP_INDEX=
+#export PEG_IN_TXID=
+#export JSON_PATH=
 
 cargo run --release
